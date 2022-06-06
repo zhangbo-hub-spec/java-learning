7d1e997d081ee631fd3b965a404522ae62465a0e
zhangbo-hub-spec <87057207+zhangbo-hub-spec@users.noreply.github.com>
2022-06-06 16:24:03 +0800

    乘法表

---
+++ src/MultiTable.java
@@ -0,0 +1,12 @@
+public class MultiTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            String result = "";
+            for (int j = 1; j <= i; j++) {
+
+                result += +j + "*" + i + "=" + (j * i) + "\t";
+            }
+            System.out.println(result);
+        }
+    }
+}
