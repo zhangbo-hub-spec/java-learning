772c052d126c838edbf3fc436f188ebb848c8462
zhangbo-hub-spec <87057207+zhangbo-hub-spec@users.noreply.github.com>
2022-06-14 20:05:50 +0800

    week2

--- src/week1/MultiTable.java
+++ src/week1/MultiTable.java
@@ -1,3 +1,5 @@
+package week1;
+
 public class MultiTable {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
--- src/week1/helloworld.java
+++ src/week1/helloworld.java
@@ -1,3 +1,5 @@
+package week1;
+
 public class helloworld {
     public  static void main(String[] args){
         System.out.println("hello world");
